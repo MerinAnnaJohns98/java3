54be40da5c9ee3a3c10a9da44ab83175c4b11acc
MerinAnnaJohns98 <merinannajohns2005@gmail.com>
2025-07-19 17:36:16 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Sum{
+    public static void main(String[] args){
+        //Author:Merin Anna Johns
+        System.out.println("enter the two nos:");
+        Scanner sc=new Scanner(System.in);
+        int n1,n2;
+        System.out.println("enter the first number:");
+        n1=sc.nextInt();
+        System.out.println("enter the second number:");
+        n2=sc.nextInt();
+        int sum=n1+n2;
+  System.out.println("Sum is:"+sum);
+    }
+}
